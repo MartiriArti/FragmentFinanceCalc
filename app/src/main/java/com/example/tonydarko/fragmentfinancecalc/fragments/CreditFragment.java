50fec162916a270f8efed9593e2bd7fb9b9c4495
TonyDarko <antonyo007h@gmail.com>
2018-01-11 10:47:18 +0200

    //

--- app/src/main/java/com/example/tonydarko/fragmentfinancecalc/fragments/CreditFragment.java
+++ app/src/main/java/com/example/tonydarko/fragmentfinancecalc/fragments/CreditFragment.java
@@ -2,6 +2,7 @@ package com.example.tonydarko.fragmentfinancecalc.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -44,18 +45,68 @@ public class CreditFragment extends Fragment {
     @BindView(R.id.tv_itogovaya_summa)
     TextView textViewItogovaya;
 
+    float sumOfCredit = 0f, stavka = 0f, duration = 0f, strahovka = 0f, avans = 0f, komissiya = 0f,
+            vznos, mesPlatej, pereplata, itogovaya;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_credit, container, false);
-        ButterKnife.bind(this,root);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Credit Calc");
+        ButterKnife.bind(this, root);
         return root;
     }
 
     @OnTextChanged({R.id.edit_text_sum_credit, R.id.edit_text_stavka, R.id.edit_text_duration,
             R.id.edit_text_strahovka, R.id.edit_text_avans, R.id.edit_text_komissiya})
-    public void onTextChange(Editable e){
-        Toast.makeText(getContext(), e, Toast.LENGTH_SHORT).show();
+    public void onTextChange(Editable e) {
+
+        if (!editTextSumOfCredit.getText().toString().equals("")) {
+            sumOfCredit = Float.parseFloat(editTextSumOfCredit.getText().toString());
+        }
+        if (!editTextAvan.getText().toString().equals("")) {
+            avans = Float.parseFloat(editTextAvan.getText().toString());
+        }
+        if (!editTextStavka.getText().toString().equals("")) {
+            stavka = Float.parseFloat(editTextStavka.getText().toString());
+        }
+        if (!editTextDuration.getText().toString().equals("")) {
+            duration = Float.parseFloat(editTextDuration.getText().toString());
+        }
+        if (!editTextStrahovka.getText().toString().equals("")) {
+            strahovka = Float.parseFloat(editTextStrahovka.getText().toString());
+        }
+        if (!editTextKomissiya.getText().toString().equals("")) {
+            komissiya = Float.parseFloat(editTextKomissiya.getText().toString());
+        }
+
+        vznos = vznos(sumOfCredit, avans);
+        textViewVznos.setText(vznos + "");
+
+        mesPlatej = mesPlatej(vznos, sumOfCredit, duration, stavka, strahovka, komissiya);
+        textViewMesPlatej.setText(mesPlatej + "");
+
+        textViewPereplata.setText(pereplata + "");
+
+        itogovaya = (sumOfCredit) + (mesPlatej * duration);
+        textViewItogovaya.setText(itogovaya + "");
+
+
     }
+
+    public float vznos(float sumOfCredit, float avans) {
+        return sumOfCredit * 0.01f * avans;
+    }
+
+    public float mesPlatej(float vznos, float sumOfCredit, float duration,
+                           float stavka, float strahovka, float komissiya) {
+        sumOfCredit = sumOfCredit - vznos;
+        float mesyachniy = sumOfCredit / duration;
+        float mesyachniyWithPercents = mesyachniy * 0.01f * stavka * strahovka * komissiya;
+        pereplata = (mesyachniyWithPercents) * duration;
+        return mesyachniyWithPercents;
+    }
+
 
 }
--- app/src/main/java/com/example/tonydarko/fragmentfinancecalc/fragments/DepositFragment.java
+++ app/src/main/java/com/example/tonydarko/fragmentfinancecalc/fragments/DepositFragment.java
@@ -1,7 +1,9 @@
 package com.example.tonydarko.fragmentfinancecalc.fragments;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -47,24 +49,74 @@ public class DepositFragment extends Fragment {
     @BindView(R.id.tv_itogovaya_summa)
     TextView textViewItogovayaSumma;
 
-    float sumVklada = 0f, stavka = 0f, duration = 0f, popolnenie = 0f, nalog = 0f;
+    float sumVklada = 0f, stavka = 0f, duration = 0f, popolnenie = 0f, nalog = 0f,
+            mesDohod, obshiyDohod, sumPopolneniy, itogovaya;
+    Boolean kapitalization = false;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_deposit, container, false);
-        ButterKnife.bind(this,root);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Deposit Calc");
+        ButterKnife.bind(this, root);
         return root;
     }
 
     @OnTextChanged({R.id.edit_text_nalog, R.id.edit_text_popolnenie, R.id.edit_text_duration,
             R.id.edit_text_stavka, R.id.edit_text_summa_vklada})
-    public void onTextChange(Editable s){
-        Toast.makeText(getContext(), s, Toast.LENGTH_SHORT).show();
+    public void onTextChange(Editable s) {
+        if (!editTextSummaVklada.getText().toString().equals(""))
+            sumVklada = Float.parseFloat(editTextSummaVklada.getText().toString());
+        if (!editTextStavka.getText().toString().equals(""))
+            stavka = Float.parseFloat(editTextStavka.getText().toString());
+        if (!editTextDuration.getText().toString().equals(""))
+            duration = Float.parseFloat(editTextDuration.getText().toString());
+        if (!editTextPopolnenie.getText().toString().equals(""))
+            popolnenie = Float.parseFloat(editTextPopolnenie.getText().toString());
+        if (!editTextNalog.getText().toString().equals(""))
+            nalog = Float.parseFloat(editTextNalog.getText().toString());
+
+        mesDohod = mesDohod(sumVklada, stavka, nalog);
+        textViewMesDohod.setText(mesDohod + "");
+
+        obshiyDohod = obshiyDohod(sumVklada, stavka, nalog, duration);
+        textViewObshiyDohod.setText(obshiyDohod + "");
+
+        sumPopolneniy = popolnenie * duration;
+        textViewObshayaSummaPopolneniy.setText(sumPopolneniy + "");
+
+        itogovaya = sumVklada + obshiyDohod + sumPopolneniy;
+        textViewItogovayaSumma.setText(itogovaya + "");
+
+    }
+
+    public float mesDohod(float sumVklada, float stavka, float nalog) {
+        float dohod = sumVklada * 0.01f * stavka;
+        float dohodWithoutNalog = dohod * 0.01f * nalog;
+        return dohod - dohodWithoutNalog;
+    }
+
+    public float obshiyDohod(float sumVklada, float stavka, float nalog, float duration) {
+        float dohodChistiy = 0f;
+
+        for (int i = 0; i <= duration; i++) {
+            float dohod = sumVklada * 0.01f * stavka;
+            float dohodWithoutNalog = dohod * 0.01f * nalog;
+            if (kapitalization) {
+                sumVklada += dohodWithoutNalog;
+            }
+            dohodChistiy += dohodWithoutNalog;
+        }
+
+        return dohodChistiy;
     }
 
     @OnCheckedChanged(R.id.checkbox_kap)
-    public void onChecked(){
-        Toast.makeText(getContext(), "Check", Toast.LENGTH_SHORT).show();
+    public void onChecked() {
+        if (checkBox.isChecked()) {
+            kapitalization = true;
+        } else if (!checkBox.isChecked()) {
+            kapitalization = false;
+        }
     }
 }
